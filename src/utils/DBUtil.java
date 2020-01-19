bb9cde9f39b2855f274d1fbaa9b0a3feda517842
h.f.sumple <atc.36@gmail.com>
2020-01-19 13:44:32 +0900

    modify spell of  daily_report_system

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,16 +5,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PESISTENCE_UNIT_NAME = "daily_report_sysytem";
+    private static final String PERSISTENCE_UNIT_NAME = "daily_report_system";
     private static EntityManagerFactory emf;
 
-    public static EntityManager createEntitymanager() {
+    public static EntityManager createEntityManager() {
         return _getEntityManagerFactory().createEntityManager();
     }
 
     public static EntityManagerFactory _getEntityManagerFactory() {
         if(emf == null){
-            emf = Persistence.createEntityManagerFactory(PESISTENCE_UNIT_NAME);
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
         return emf;
     }
